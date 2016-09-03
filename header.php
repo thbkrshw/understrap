bcf1b51b0ec278402220a41716e994518bfdac54
Harold Leboulanger <harold.leboulanger@gmail.com>
2016-09-03 23:21:50 +0200

    Add logo

--- header.php
+++ header.php
@@ -47,7 +47,7 @@
                                 </button>
 
                                 <!-- Your site title as branding in the menu -->
-                                <a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+    <a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img alt="<?php bloginfo( 'name' ); ?>" src="<?php bloginfo('template_directory'); ?>/img/logo.png" /></a>
 
                             </div>
 
